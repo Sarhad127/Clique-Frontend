a39f15b37544e73cd595bd18b7438619c1b89b83
Sarhad Bahrami <Sarhad94127@hotmail.com>
2025-06-04 00:25:30 +0200

    update 1

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
 import './styles/Login.css'
 import CliqueIcon from './icons/Clique-icon.png'
+import {useNavigate} from "react-router-dom";
 
 function Login() {
+    const navigate = useNavigate();
+
     return(
         <>
             <div className="main-container">
@@ -11,6 +14,38 @@ function Login() {
             </div>
                 <div className="main-login">
                     <div className="login-title">Login</div>
+
+                    <section className="login-form">
+                        <form>
+                            <div className="input-group">
+                                <input
+                                    type="text"
+                                    id="usernameOrEmail"
+                                    name="usernameOrEmail"
+                                    placeholder="Enter your username or email"
+                                    required
+                                />
+                                <label htmlFor="usernameOrEmail">Username or Email</label>
+                            </div>
+                            <div className="input-group">
+                                <input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    placeholder="Enter your password"
+                                    required
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                            <div className="button-group">
+                                <button type="submit" className="login-button">Log In</button>
+                                <button type="submit" className="register-button"
+                                        onClick={() => navigate('/register')}
+                                >Sign Up</button>
+                            </div>
+                        </form>
+                    </section>
+
                 </div>
             </div>
         </>
